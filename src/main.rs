6f42b625bfc4ffd23e5d855fa543126b304eaf6f
MiguelX413 <lbpgaming27@gmail.com>
2023-05-12 04:57:59 +0000

    Make formatting and stylistic adjustments, make more idiomatic, fix some logic

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,59 @@
 #[allow(dead_code)]
 
-fn is_valid_keyword(s:&str)->bool{
-    return ["if","while","else","elif","endif","endwhile"].contains(&s);
+fn is_valid_keyword(s: &str) -> bool {
+    matches!(s, "if" | "while" | "else" | "elif" | "endif")
 }
-fn is_valid_symbol(c:char)->bool{
-    return ['+','-','*','/','='].contains(&c);
+fn is_valid_symbol(c: char) -> bool {
+    matches!(c, '+' | '-' | '*' | '/' | '=')
 }
-fn is_valid_token(s: &str)->bool{
-   return s.is_empty() || s.chars().all(char::is_alphanumeric) || is_valid_keyword(s);
+fn is_valid_token(s: &str) -> bool {
+    s.is_empty() || s.chars().all(char::is_alphanumeric) || is_valid_keyword(s)
 }
 
-fn find_delimiter <F> (s:&str, op:F)-> (&str,&str) where F: Fn(char)->bool, {
-    let delim = s.chars().position(op);
+fn find_delimiter<F>(s: &str, op: F) -> (&str, &str)
+where
+    F: Fn(char) -> bool,
+{
+    let delim = s.find(op);
     match delim {
-        Some(delim)=>{
-            let (first_token,remainder) = s.split_at(delim);
-            return (first_token,remainder);
+        Some(delim) => {
+            let (first_token, remainder) = s.split_at(delim);
+            (first_token, remainder)
         }
-        None => {return (s," ");} 
-    } 
-} 
+        None => (s, " "),
+    }
+}
 
-fn get_first_token(str:&str)->(&str,&str){
+fn get_first_token(str: &str) -> (&str, &str) {
     let s = str.trim();
     let first = s.chars().next();
-    match first{
-    Some(first)=>{
-            if first.is_numeric(){
-            return find_delimiter(s, |c|!c.is_numeric());
-            }
-            else if first.is_alphabetic(){
-                return find_delimiter(s, |c| ! c.is_alphanumeric())  
-            }
-            else if is_valid_symbol(first){
-                return s.split_at(1); 
-            }
-            else{
-                return (" ", " ");
+    match first {
+        Some(first) => {
+            if first.is_numeric() {
+                find_delimiter(s, |c| !c.is_numeric())
+            } else if first.is_alphabetic() {
+                find_delimiter(s, |c| !c.is_alphanumeric())
+            } else if is_valid_symbol(first) {
+                s.split_at(1)
+            } else {
+                (" ", " ")
             }
         }
-    None => {return (" ", " ");}
-    
-    }   
+        None => (" ", " "),
+    }
 }
-fn tokenize(s:&str, tokens: &mut Vec<String>)->Vec<String>{
-    let (first,remainder) = get_first_token(&s);
-    if remainder.eq(" "){
+fn tokenize(s: &str, tokens: &mut Vec<String>) -> Vec<String> {
+    let (first, remainder) = get_first_token(s);
+    if remainder == " " {
         tokens.push(first.to_string());
-        return tokens.iter().map(|s|s.to_string()).collect();
-    }else{
+        tokens.iter().map(|s| s.to_string()).collect()
+    } else {
         tokens.push(first.to_string());
-        return tokenize(&remainder, tokens);
+        tokenize(remainder, tokens)
     }
 }
 fn main() {
     let mut tokens = Vec::new();
     let result = tokenize("if   +-/123asspenis11", &mut tokens);
-    result.iter().for_each(|x|println!("{}",x))
+    result.iter().for_each(|x| println!("{}", x))
 }
