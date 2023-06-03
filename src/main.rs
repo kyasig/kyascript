25c23c280cd8989e60d1b4195c2d620892cee98f
kyasig <92475855+kyasig@users.noreply.github.com>
2023-06-02 22:03:05 -0700

    changed dumb sentinal value code

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn is_valid_token(s: &str) -> bool {
         || is_valid_operator(s)
 }
 
-fn find_delimiter<F>(s: &str, op: F) -> (&str, &str)
+fn find_delimiter<F>(s: &str, op: F) -> (&str, Option<&str>)
 where
     F: Fn(char) -> bool,
 {
@@ -25,41 +25,48 @@ where
     match delim {
         Some(delim) => {
             let (first_token, remainder) = s.split_at(delim);
-            (first_token, remainder)
+            (first_token, Some(remainder))
         }
-        None => (s, " "),
+        None => (s, None),
     }
 }
 
-fn get_first_token(str: &str) -> (&str, &str) {
+
+fn get_first_token(str: &str) -> (&str, Option<&str>) {
     let s = str.trim();
     let first = s.chars().next();
     match first {
         Some(first) => {
             if !is_valid_token(&first.to_string()) {
                 panic!("invalid token at{:?}", first);
-            } else if first.is_numeric() {
+            } 
+            else if first.is_numeric() {
                 find_delimiter(s, |c| !c.is_numeric())
             } else if first.is_alphabetic() {
                 find_delimiter(s, |c| !c.is_alphanumeric())
             } else if is_valid_symbol(&first.to_string()) || is_valid_operator(&first.to_string()) {
-                s.split_at(1)
-            } else {
-                ("", "")
-            }
+                (&s[0..1], Some(&s[1..]))
+            }else{
+                panic!("wtf idk what to do here ngl")
+            } 
         }
-        None => (s, ""),
+        None => (s, None)
     }
 }
 fn tokenize(s: &str) -> Vec<&str> {
     let mut tokens: Vec<&str> = Vec::new();
     let (mut first, mut remainder) = get_first_token(&s);
-    while remainder != "" {
+    loop{
         tokens.push(first);
-        (first, remainder) = get_first_token(remainder);
-    }
+        if remainder == None {
+            break;
+        }else{
+            (first,remainder) = get_first_token(remainder.unwrap());
+        }
+    } 
     tokens
 }
+
 
 //////AST stuff//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #[derive(Debug)]
